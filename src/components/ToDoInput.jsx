169898e6c67fb8b70f73cc4b6a5806fd554e0c80
baharkose <baharkse17@gmail.com>
2024-03-08 09:50:42 +0300

    feat: added dispatch and todoReducer to input

--- src/components/ToDoInput.jsx
+++ src/components/ToDoInput.jsx
@@ -1,14 +1,31 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
+// burada tanımladığımız reducerımızı çağırdık. Add işlemi için
+import { addTodo } from "../features/todoReducer";
 
 const ToDoInput = () => {
   const [text, setText] = useState("");
   const dispatch = useDispatch();
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // sonra bir dispatch yayınladık ki state değişsin
+    dispatch(addTodo(text));
+    setText("");
+  };
+
+  console.log(text);
+
   return (
     <>
       <form onSubmit={handleSubmit}>
-        <input type="text" />
+        <input
+          type="text"
+          placeholder="Add Todo"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <button type="submit">Add</button>
       </form>
     </>
   );
