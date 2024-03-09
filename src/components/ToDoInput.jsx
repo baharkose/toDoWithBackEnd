1fa77d621068e3cc6f02e26187e7b80aa30bdeb8
baharkose <baharkse17@gmail.com>
2024-03-09 23:20:13 +0300

    feat: show hide btn, input elements

--- src/components/ToDoInput.jsx
+++ src/components/ToDoInput.jsx
@@ -18,21 +18,24 @@ const ToDoInput = () => {
 
   return (
     <>
-      <form onSubmit={handleSubmit} >
-        <input
-          type="Enter Title"
-          placeholder="Add Todo"
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-        />
-        <input
-          type="Enter Title"
-          placeholder="Add Todo"
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-        />
-        <button type="submit">Add</button>
-      </form>
+      <div className="">
+        <form onSubmit={handleSubmit}>
+          
+          <input
+            type="Enter Title"
+            placeholder="Add Todo"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+          <input
+            type="Enter Title"
+            placeholder="Add Todo"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+          <button type="submit">Add</button>
+        </form>
+      </div>
     </>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import ToDoInput from "../components/ToDoInput";
 import TodoList from "../components/TodoList";
 
 const Home = () => {
+  const [btnToogle, setBtnToogle] = useState(false);
+
+  const handleToggle = () => {
+    setBtnToogle(!btnToogle);
+  };
   return (
-    <div>
-      <h2>TASK TRACKER</h2>
-      <ToDoInput />
-      <TodoList />
+    <div className="h-screen flex  justify-center  bg-red-50 pt-32">
+      <div className="flex bg-red-50 pt-10 items-center flex-col space-x-16 border-2 gap-3 space-x-0 p-2 min-w-96">
+        <h2 className="text-orange-700 text-4xl">TASK TRACKER</h2>
+
+        <button
+          onClick={handleToggle}
+          className="bg-red-600 pr-16 pl-16 pt-3 pb-3 text-white rounded-md"
+        >
+          {btnToogle ? "Hide Task Bar" : "Show Task Bar"}
+        </button>
+        {btnToogle && <ToDoInput />}
+        {btnToogle && <TodoList />}
+      </div>
     </div>
   );
 };
